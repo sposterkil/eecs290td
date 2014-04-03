2238235db1c5783cdf775ea17b2408b732f7b2bf
Zanice <znj@case.edu>
2014-04-03 16:09:27 -0400

    Multi-Enemy Turret Targeting Based on Proxy *TEMP

--- Main/Assets/Turrets/TurretScript.cs
+++ Main/Assets/Turrets/TurretScript.cs
@@ -12,10 +12,28 @@ public class TurretScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Look at target
-		Transform enemy = GameObject.Find("TestEnemy").transform;
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 		if (enemies.Length == 1) {
 			turret.LookAt(enemies[0].transform.position);
+		}
+		else {
+			Transform enemy;
+			Vector3 dist;
+			int indexOfLowest = -1;
+			float distOfLowest = 0f;
+			for (int i = 0; i < enemies.Length; i++) {
+				enemy = enemies[i].transform;
+				dist = new Vector3(enemy.position.x - transform.position.x, enemy.position.y - transform.position.y, enemy.position.z - transform.position.z);
+				if (i == 0) {
+					indexOfLowest = 0;
+					distOfLowest = dist.magnitude;
+				}
+				else if (dist.magnitude < distOfLowest) {
+					indexOfLowest = i;
+					distOfLowest = dist.magnitude;
+				}
+			}
+			turret.LookAt(enemies[indexOfLowest].transform.position);
 		}
 	}
 }
