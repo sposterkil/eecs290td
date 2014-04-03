921d0f2fc826715e20240741f2f98fff54ead45b
Zanice <znj@case.edu>
2014-04-03 15:25:29 -0400

    Basic Turret Implementation

--- Main/Assets/Turrets/TurretScript.cs
+++ Main/Assets/Turrets/TurretScript.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class TurretScript : MonoBehaviour {
+	Transform turret;
 
 	// Use this for initialization
 	void Start () {
-	
+		turret = transform.FindChild("Turret").FindChild("TurretBody").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Look at target
+		Transform enemy = GameObject.Find("TestEnemy").transform;
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		if (enemies.Length == 1) {
+			turret.LookAt(enemies[0].transform.position);
+		}
 	}
 }
