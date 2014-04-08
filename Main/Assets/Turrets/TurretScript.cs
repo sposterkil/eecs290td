b07676c16a0cda85a06dfb7676d073c9eb07d395
Zanice <znj@case.edu>
2014-04-08 12:33:54 -0400

    Changed Turret Aiming to defend "Beacon"

--- Main/Assets/Turrets/TurretScript.cs
+++ Main/Assets/Turrets/TurretScript.cs
@@ -16,27 +16,52 @@ public class TurretScript : MonoBehaviour {
 	void Update () {
 		//Look at target
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject[] beacons = GameObject.FindGameObjectsWithTag("Beacon");
 		if (enemies.Length == 1) {
 			target = enemies[0];
 		}
 		else if (enemies.Length != 0) {
-			Transform enemy;
-			Vector3 dist;
-			int indexOfLowest = -1;
-			float distOfLowest = 0f;
-			for (int i = 0; i < enemies.Length; i++) {
-				enemy = enemies[i].transform;
-				dist = new Vector3(enemy.position.x - transform.position.x, enemy.position.y - transform.position.y, enemy.position.z - transform.position.z);
-				if (i == 0) {
-					indexOfLowest = 0;
-					distOfLowest = dist.magnitude;
+			//Have tower aim via distance from beacon for a single beacon.
+			if (beacons.Length == 1) {
+				Transform enemy;
+				Transform beacon = beacons[0].transform;
+				Vector3 dist;
+				int indexOfLowest = -1;
+				float distOfLowest = 0f;
+				for (int i = 0; i < enemies.Length; i++) {
+					enemy = enemies[i].transform;
+					dist = new Vector3(enemy.position.x - beacon.position.x, enemy.position.y - beacon.position.y, enemy.position.z - beacon.position.z);
+					if (i == 0) {
+						indexOfLowest = 0;
+						distOfLowest = dist.magnitude;
+					}
+					else if (dist.magnitude < distOfLowest) {
+						indexOfLowest = i;
+						distOfLowest = dist.magnitude;
+					}
 				}
-				else if (dist.magnitude < distOfLowest) {
-					indexOfLowest = i;
-					distOfLowest = dist.magnitude;
-				}
+				target = enemies[indexOfLowest];
 			}
-			target = enemies[indexOfLowest];
+			//Otherwise, if no beacons or more than one beacon exist, revert the tower to aim via proxy.
+			else {
+				Transform enemy;
+				Vector3 dist;
+				int indexOfLowest = -1;
+				float distOfLowest = 0f;
+				for (int i = 0; i < enemies.Length; i++) {
+					enemy = enemies[i].transform;
+					dist = new Vector3(enemy.position.x - transform.position.x, enemy.position.y - transform.position.y, enemy.position.z - transform.position.z);
+					if (i == 0) {
+						indexOfLowest = 0;
+						distOfLowest = dist.magnitude;
+					}
+					else if (dist.magnitude < distOfLowest) {
+						indexOfLowest = i;
+						distOfLowest = dist.magnitude;
+					}
+				}
+				target = enemies[indexOfLowest];
+			}
 		}
 		if (target != null) {
 			turret.LookAt(target.transform.position);
