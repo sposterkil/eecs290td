af6848991a8ae1058672317bc6343e9c40accd96
Kaan Atesoglu <aka43@case.edu>
2014-04-10 16:14:43 -0400

    Added the tower grid

---
+++ Main/Assets/AI/NodeScripts.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeScripts : MonoBehaviour {
+
+	public GameObject node;
+	public float rotSpeed;
+
+	public void spawn() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		node.transform.Rotate (0, rotSpeed * Time.deltaTime, 0, Space.World);
+	}
+}
