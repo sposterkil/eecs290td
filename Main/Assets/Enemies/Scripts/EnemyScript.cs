86282598951ac37ce21f6f96a372ce4aac3ea71b
Zanice <znj@case.edu>
2014-04-10 21:05:50 -0400

    Added Attributes to Enemies for Turret Alteration

--- Main/Assets/Enemies/Scripts/EnemyScript.cs
+++ Main/Assets/Enemies/Scripts/EnemyScript.cs
@@ -3,23 +3,50 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 	public int health;
-	public int damage;
+	public float damage;
+	public long damageduration;
 	public float speed;
+	public long speedduration;
 
 	// Use this for initialization
 	void Start () {
-	
+		//System.DateTime.Now.Ticks
+		damage = 1;
+		speed = 1;
+		damageduration = -1;
+		speedduration = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (damageduration != -1) {
+			if (System.DateTime.Now.Ticks >= damageduration) {
+				damageduration = -1;
+				damage = 1;
+			}
+		}
+		if (speedduration != -1) {
+			if (System.DateTime.Now.Ticks >= speedduration) {
+				speedduration = -1;
+				speed = 1;
+			}
+		}
 	}
 	
 	public void takeDamage(int damage) {
 		health -= damage;
 		if (health <= 0)
 			die();
+	}
+	
+	public void reduceDamage(float factor, float duration) {
+		damage = factor;
+		damageduration = (System.DateTime.Now.Ticks + (1000L * 10000L)) * duration;
+	}
+	
+	public void reduceSpeed(float factor, float duration) {
+		speed = factor;
+		speedduration = (System.DateTime.Now.Ticks + (1000L * 10000L)) * duration;
 	}
 	
 	void die() {
