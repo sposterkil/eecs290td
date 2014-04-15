bb54253b2ec0910ad61f0e05fdeac51ddbe523bb
Sam Osterkil <pisketch@gmail.com>
2014-04-15 19:45:34 -0400

    Added Callum's MouseOrbit script, for camera tracking

---
+++ Main/Assets/Towers/Master Tower/MouseOrbit.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseOrbit : MonoBehaviour
+{
+	public Transform target;
+	public float distance = 0.0f;
+
+	float centerX = Screen.width / 2;
+	float centerY = Screen.height / 2;
+
+	float mouseX = Input.mousePosition.x;
+	float mouseY = Input.mousePosition.y;
+
+	public float xSpeed = 0.1f;
+	public float ySpeed = 0.1f;
+
+	public float yMinLimit = -20;
+	public float yMaxLimit = 80;
+
+	private float x = 0.0f;
+	private float y = 0.0f;
+
+	void Start ()
+	{
+		var angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+
+		// Make the rigid body not change rotation
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+
+	void Update ()
+	{
+		mouseX = Input.mousePosition.x;
+		mouseY = Input.mousePosition.y;
+		if (target)
+		{
+			x += (mouseX - centerX) * xSpeed * Time.deltaTime;
+			y -= (mouseY - centerY) * ySpeed * Time.deltaTime;
+
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+			transform.rotation = Quaternion.Euler(y, x, 0);
+			// transform.position = (Quaternion.Euler(y, x, 0)) * new Vector3(0.0f, 0.0f, -distance) + target.position;
+		}
+	}
+
+	static float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360)
+		{
+			angle += 360;
+		}
+		if (angle > 360)
+		{
+			angle -= 360;
+		}
+		return Mathf.Clamp(angle, min, max);
+	}
+}
