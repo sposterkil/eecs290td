07ea34d2ff5d425abc04dbf9a42c330d92fbf6dd
Zanice <znj@case.edu>
2014-04-15 14:42:56 -0400

    Added AOE Tower Script, still to complete

---
+++ Main/Assets/Towers/Scripts/WipeTower.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WipeTower : MonoBehaviour {
+	TowerManager manager;
+	Transform turret;
+	Transform target;
+
+	public long cooldownTimer;
+
+	public int damage;
+	public int cooldown;
+	public float range;
+	public float reduxDamage;
+	public int reduxDamageDuration;
+	public float reduxSpeed;
+	public int reduxSpeedDuration;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find("Towers").GetComponent<TowerManager>();
+		turret = transform.FindChild("Turret").FindChild("Turret1").transform;
+		target = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = manager.findTarget(transform, range);		
+		if (target != null) {
+			turret.LookAt(target.transform.position);
+			if (System.DateTime.Now.Ticks >= cooldownTimer) {
+				//Apply damage
+				target.GetComponent<EnemyScript>().takeDamage(damage);
+				//Apply damage reduction if applicable
+				if (reduxDamage != 0) {
+					target.GetComponent<EnemyScript>().reduceDamage(reduxDamage, reduxDamageDuration);
+				}
+				//Apply movement speed reduction if applicable
+				if (reduxSpeed != 0) {
+					target.GetComponent<EnemyScript>().reduceSpeed(reduxSpeed, reduxSpeedDuration);
+				}
+				cooldownTimer = System.DateTime.Now.Ticks + (10000 * cooldown);
+				Debug.DrawLine(turret.position, target.position, new Color(0, 0, 0, 255));
+			}
+		}
+		else
+			turret.LookAt(turret.position + new Vector3(0, 0, -1));
+	}
+}
