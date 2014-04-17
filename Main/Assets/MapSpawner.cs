23e79483065e6fe99c7c53d5c8078c666b20ee54
Sam Osterkil <pisketch@gmail.com>
2014-04-17 15:38:37 -0400

    Changed floor grid to be texture based

--- Main/Assets/MapSpawner.cs
+++ Main/Assets/MapSpawner.cs
@@ -7,41 +7,33 @@ public class MapSpawner : MonoBehaviour {
 	public Transform spawn2;
 	public Transform spawn3;
 
-	public GameObject virus1;
-	public GameObject virus2;
-
-	public AudioSource spawnSound;
+	public GameObject virus;
 
 	void Update() {
 
 		// spawn a random virus
 		if (Input.GetKeyDown (KeyCode.F2)){
-			spawnSound.Play ();
-			spawnVirus1();
-		}
-		if (Input.GetKeyDown (KeyCode.F3)){
-			spawnSound.Play ();
-			spawnVirus2();
+			spawnVirus();
 		}
 	}
 
 
 	// spawns a virus at a random spawn
-	public void spawnVirus1(){
+	public void spawnVirus(){
 		GameObject clone;
 		int random = Random.Range(1, 4);
 		switch (random) {
 			case 1:
-				Debug.Log ("spawn1 @ 1");
-				clone = Instantiate(virus1, spawn1.position , Quaternion.identity) as GameObject;
+				Debug.Log ("spawn @ 1");
+				clone = Instantiate(virus, spawn1.position , Quaternion.identity) as GameObject;
 			break;
 			case 2:
-				Debug.Log ("spawn1 @ 1");
-				clone = Instantiate(virus1, spawn2.position , Quaternion.identity) as GameObject;
+				Debug.Log ("spawn @ 1");
+				clone = Instantiate(virus, spawn2.position , Quaternion.identity) as GameObject;
 			break;
 			case 3:
-				Debug.Log ("spawn1 @ 3");
-				clone = Instantiate(virus1, spawn3.position , Quaternion.identity) as GameObject;
+				Debug.Log ("spawn @ 3");
+				clone = Instantiate(virus, spawn3.position , Quaternion.identity) as GameObject;
 			break;
 			default:
 				Debug.Log ("defualt spawn, broken");
@@ -50,29 +42,9 @@ public class MapSpawner : MonoBehaviour {
 
 	}
 
-	// spawns a virus at a random spawn
-	public void spawnVirus2(){
-		GameObject clone;
-		int random = Random.Range(1, 4);
-		switch (random) {
-		case 1:
-			Debug.Log ("spawn2 @ 1");
-			clone = Instantiate(virus2, spawn1.position , Quaternion.identity) as GameObject;
-			break;
-		case 2:
-			Debug.Log ("spawn2 @ 1");
-			clone = Instantiate(virus2, spawn2.position , Quaternion.identity) as GameObject;
-			break;
-		case 3:
-			Debug.Log ("spawn2 @ 3");
-			clone = Instantiate(virus2, spawn3.position , Quaternion.identity) as GameObject;
-			break;
-		default:
-			Debug.Log ("defualt spawn, broken");
-			break;
+	// spawns a virus at a specific spawn place
+	public void spawnVirus(int position){
 		}
-		
-	}
 
 
 }
