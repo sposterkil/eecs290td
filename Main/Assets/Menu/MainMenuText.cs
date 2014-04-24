6e9f9b5998ed7b8ecc627a4850b1109a9dbef746
Zanice <znj@case.edu>
2014-04-24 14:51:16 -0400

    Title Scene

---
+++ Main/Assets/Menu/MainMenuText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuText: MonoBehaviour 
+{
+	public bool isQuit;
+	public bool isStart;
+	
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.gray;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+
+	void OnMouseDown() {
+		renderer.material.color = Color.red;
+
+		if(isStart) {
+			Application.LoadLevel(1);
+		}
+		else if(isQuit) {
+			Application.Quit();
+		}
+		else {
+		}
+	}
+}
