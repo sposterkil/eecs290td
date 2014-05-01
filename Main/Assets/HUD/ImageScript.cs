03b59e0654a6c9823045257e2193a880fb115cd7
Kaan Atesoglu <aka43@case.edu>
2014-04-30 20:18:52 -0400

    Tower Selector Text working

--- Main/Assets/HUD/ImageScript.cs
+++ Main/Assets/HUD/ImageScript.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ImageScript : MonoBehaviour {
 
-	public GameObject pane;
-	public TextMesh   text;
+	public GameObject pane; // the image pane
+	public TextMesh   text; // 
 	public MasterTower masterTowerScript;
 
 	public void Update () {
@@ -12,33 +12,35 @@ public class ImageScript : MonoBehaviour {
 	}
 
 	public void setTower(GameObject tower) {
-
 		switch (tower.ToString ()) {
-		case "ThreadTower":
+		case "ThreadTower (UnityEngine.GameObject)":
 			text.text = "Thread - Simple Single Target";
 			break;
-		case "GlitchTower":
+		case "GlitchTower (UnityEngine.GameObject)":
 			text.text = "Glitch - Lower Damage + Stun";
 			break;
-		case "GarbageCollectionTower":
+		case "GarbageCollectionTower (UnityEngine.GameObject)":
 			text.text = "Garbage Collection - Bonus RAM";
 			break;
-		case "ProcessModeratorTower":
+		case "ProcessModeratorTower (UnityEngine.GameObject)":
 			text.text = "Process Mod - Targets Highest Health";
 			break;
-		case "BreakTower":
+		case "ProcessTerminatorTower (UnityEngine.GameObject)":
+			text.text = "Process Terminator - Targets Lowest Health";
+			break;
+		case "BreakTower (UnityEngine.GameObject)":
 			text.text = "Break - Single shot, High Range, High Damage";
 			break;
-		case "WipeTower":
+		case "WipeTower (UnityEngine.GameObject)":
 			text.text = "Wipe - Deals Area Damage";
 			break;
-		case "LagTower":
+		case "LagTower (UnityEngine.GameObject)":
 			text.text = "Lag - Slow Area Around Tower";
 			break;
-		case "MemoryTower":
-			text.text = "Memory - Fires Faster with Succsessive Shots";
+		case "MemoryTower (UnityEngine.GameObject)":
+			text.text = "Memory - Increasing Fire Rate";
 			break;
-		case "LoadTower":
+		case "LoadTower (UnityEngine.GameObject)":
 			text.text = "Load - Area Damage Devided Amoung Targets";
 			break;
 		}
